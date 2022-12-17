6a21520bbe1db3267c07c4ee6fe309aec171671b
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2022-12-17 10:02:59 -0300

    test finish

--- users/views.py
+++ users/views.py
@@ -15,33 +15,4 @@ class UserDetailView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAccountOwner]
         
 
-from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
-from .models import User
 
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = [
-            "id",
-            "username",
-            "email",
-            "first_name",
-            "last_name",
-            "is_superuser",
-            "password",
-        ]
-        extra_kwargs = {
-            "password": {"write_only": True},
-        }
-
-    def create(self, validated_data: dict) -> User:
-        return User.objects.create_superuser(**validated_data)
-
-    def validate_email(self, value: str):
-        try:
-            User.objects.get(email=value)
-            raise serializers.ValidationError("This field must be unique.")
-        except User.DoesNotExist:
-            return value
